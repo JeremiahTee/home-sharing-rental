054c68a1cb64e5ced64041cd4f001324ca80bc3d
Jeremiah Tiongson <jeremiahteee@gmail.com>
2023-06-06 12:08:03 -0400

    initial server with basic routes
    to continue development on Mac

---
+++ tinyhouse_v1/server/src/index.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { listings } from './listings';
+const app = express();
+const port = 9000;
+
+app.use(bodyParser.json()); //help parse incoming requests as JSON, expose resulting object on request.body
+
+app.get('/listings', (_req, res) => {
+    return res.send(listings)
+})
+
+app.post('/delete-listing', (req, res) => {
+    const id: string = req.body.id;
+
+    for(let i = 0; i < listings.length; i++) {
+        if (listings[i].id === id) {
+            return res.send(listings.splice(i, 1));
+        }
+    }
+
+    return res.send('failed to delete listing');
+})
+
+app.listen(port);
+
+console.log(`[app]: http://localhost:${port}`);
