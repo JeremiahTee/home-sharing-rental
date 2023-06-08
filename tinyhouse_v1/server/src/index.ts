c8ec972d693f607b82c6e7c664c9adb1cdd7540f
Jeremiah Tiongson <jeremiahteee@gmail.com>
2023-06-07 21:01:39 -0400

    refactor: use graphQL schema language, resolvers

--- tinyhouse_v1/server/src/index.ts
+++ tinyhouse_v1/server/src/index.ts
@@ -1,10 +1,10 @@
 import express, { Application } from "express";
 import { ApolloServer } from "apollo-server-express";
-import { schema } from "./graphql";
+import { typeDefs, resolvers } from "./graphql";
 
 async function mount(app: Application) {
   const port = 9000;
-  const server = new ApolloServer({ schema });
+  const server = new ApolloServer({ typeDefs, resolvers });
   await server.start();
   server.applyMiddleware({ app, path: "/api" });
 
