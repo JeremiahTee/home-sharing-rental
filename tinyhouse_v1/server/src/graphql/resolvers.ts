c79fc7716bc6a9be82ed21002edd54cd5cba84f1
Jeremiah Tiongson <jeremiahteee@gmail.com>
2023-06-08 00:14:48 -0400

    update resolvers to process data from mongodb

--- tinyhouse_v1/server/src/graphql/resolvers.ts
+++ tinyhouse_v1/server/src/graphql/resolvers.ts
@@ -1,23 +1,38 @@
+import { Database, Listing } from "./../lib/types";
 // interface types to better define types of a resolver map
 import { IResolvers } from "@graphql-tools/utils";
-import { listings } from "../listings";
+import { ObjectId } from "mongodb";
 
 // resolvers object is a map that relates schema fields to functions that resolve those fields
 export const resolvers: IResolvers = {
   Query: {
-    listings: () => {
-      return listings;
+    listings: async (
+      _root: undefined,
+      _args: {},
+      { db }: { db: Database }
+    ): Promise<Listing[]> => {
+      return await db.listings.find({}).toArray();
     }
   },
   Mutation: {
-    deleteListing: (_root: undefined, { id }: { id: string }) => {
-      for (let i = 0; i < listings.length; i++) {
-        if (listings[i].id === id) {
-          return listings.splice(i, 1)[0];
-        }
+    deleteListing: async (
+      _root: undefined,
+      { id }: { id: string },
+      { db }: { db: Database }
+    ): Promise<Listing> => {
+      const deletedResult = await db.listings.findOneAndDelete({
+        _id: new ObjectId(id)
+      });
+
+      if (!deletedResult.value) {
+        throw new Error("failed to delete listing");
       }
 
-      throw new Error("failed to delete listing");
+      return deletedResult.value;
     }
+  },
+  // Resolver to explicitly map ID
+  Listing: {
+    id: (listing: Listing): string => listing._id.toString() // _id is type of objectId
   }
 };
