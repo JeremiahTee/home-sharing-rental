1855c71aad282e0d170925912bb6386bf8c56de4
Jeremiah Tiongson <jeremiahteee@gmail.com>
2023-06-07 22:31:01 -0400

    MongoDB setup

--- tinyhouse_v1/server/src/index.ts
+++ tinyhouse_v1/server/src/index.ts
@@ -1,16 +1,28 @@
+process.env.NODE_ENV !== "production" && require("dotenv").config();
+
 import express, { Application } from "express";
 import { ApolloServer } from "apollo-server-express";
+import { connectDatabase } from "./database/index";
 import { typeDefs, resolvers } from "./graphql";
 
 async function mount(app: Application) {
-  const port = 9000;
-  const server = new ApolloServer({ typeDefs, resolvers });
+  const db = await connectDatabase();
+
+  // pass context so resolver has access to DB
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: () => ({ db })
+  });
   await server.start();
   server.applyMiddleware({ app, path: "/api" });
 
-  app.listen(port);
+  app.listen(process.env.PORT);
 
-  console.log(`[app]: http://localhost:${port}`);
+  console.log(`[app]: http://localhost:${process.env.PORT}`);
+
+  const listings = await db.listings.find({}).toArray();
+  console.log(listings);
 }
 
 mount(express());
