0e1b573510d95fb61c606b844d18696dec0ec419
JeremiahTee <jeremiahteee@gmail.com>
2023-06-08 15:02:00 -0400

    delete listing mutation

--- tinyhouse_v1/client/src/sections/Listings/Listings.tsx
+++ tinyhouse_v1/client/src/sections/Listings/Listings.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { server } from "../../lib/api";
 import { ListingsData } from "./types";
+import { DeleteListingData, DeleteListingVariables } from "./types";
 
 const LISTINGS = `
 query Listings {
@@ -17,6 +18,14 @@ query Listings {
   }
 }`;
 
+const DELETE_LISTINGS = `
+  mutation DeleteListing($id: ID!) {
+    deleteListing(id: $id) {
+      id
+    }
+  }
+`;
+
 interface Props {
   title: string;
 }
@@ -26,11 +35,26 @@ const fetchListings = async () => {
   console.log(data);
 };
 
+const deleteListing = async () => {
+  const { data } = await server.fetch<
+    DeleteListingData,
+    DeleteListingVariables
+  >({
+    query: DELETE_LISTINGS,
+    variables: {
+      id: "6481558949fbfbdd35b339e4"
+    }
+  });
+
+  console.log("deleted: ", data);
+};
+
 export const Listings = ({ title }: Props) => {
   return (
     <div>
       <h2>{title}</h2>
       <button onClick={fetchListings}>Query Listings</button>
+      <button onClick={deleteListing}>Delete a Listing</button>
     </div>
   );
 };
