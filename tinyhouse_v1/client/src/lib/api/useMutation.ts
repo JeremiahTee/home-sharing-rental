5bd72f0f143857c51e539ddade281bd1022de6c5
Jeremiah Tiongson <jeremiahteee@gmail.com>
2023-06-08 22:45:00 -0400

    feat: useReducer hook to dispatch actions manipulating state

--- tinyhouse_v1/client/src/lib/api/useMutation.ts
+++ tinyhouse_v1/client/src/lib/api/useMutation.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useReducer } from "react";
 import { server } from "./server";
 
 interface State<TData> {
@@ -7,17 +7,38 @@ interface State<TData> {
   error: boolean;
 }
 
-//tuple type which first item is a function that can accept a variables object & returns promise<void>
+type Action<TData> =
+  | { type: `FETCH` }
+  | { type: `FETCH_SUCCESS`; payload: TData }
+  | { type: `FETCH_ERROR` };
+
+// tuple type which first item is a function that can accept a variables object & returns promise<void>
 // second item is state object specified in the hook
 type MutationTuple<TData, TVariables> = [
   (variables?: TVariables | undefined) => Promise<void>,
   State<TData>
 ];
 
+const reducer =
+  <TData>() =>
+  (state: State<TData>, action: Action<TData>): State<TData> => {
+    switch (action.type) {
+      case "FETCH":
+        return { ...state, loading: true };
+      case "FETCH_SUCCESS":
+        return { data: action.payload, loading: false, error: false };
+      case "FETCH_ERROR":
+        return { ...state, loading: false, error: true };
+      default:
+        throw new Error();
+    }
+  };
+
 export const useMutation = <TData = any, TVariables = any>(
   query: string
 ): MutationTuple<TData, TVariables> => {
-  const [state, setState] = useState<State<TData>>({
+  const fetchReducer = reducer<TData>();
+  const [state, dispatch] = useReducer(fetchReducer, {
     data: null,
     loading: false,
     error: false
@@ -25,7 +46,7 @@ export const useMutation = <TData = any, TVariables = any>(
 
   const fetch = async (variables?: TVariables) => {
     try {
-      setState({ data: null, loading: true, error: false });
+      dispatch({ type: "FETCH" });
       const { data, errors } = await server.fetch<TData, TVariables>({
         query,
         variables
@@ -35,9 +56,9 @@ export const useMutation = <TData = any, TVariables = any>(
         throw new Error(errors[0].message);
       }
 
-      setState({ data, loading: false, error: false });
+      dispatch({ type: "FETCH_SUCCESS", payload: data });
     } catch (err) {
-      setState({ data: null, loading: false, error: true });
+      dispatch({ type: "FETCH_ERROR" });
       throw console.error(err);
     }
   };
